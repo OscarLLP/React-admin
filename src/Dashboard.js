17e00a5172c036fdf4a4423516d2fad9eff3a439
Unknown <oscar.lopez@imaginamos.com.co>
2018-09-07 11:02:42 -0500

    [react-admin] home

---
+++ src/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+
+export default () => (
+    <Card>
+        <CardHeader title="bienvenido a la administracion" />
+        <CardContent>Aprende sin dolor </CardContent>
+    </Card>
+
+);
+
