e64ea9bba003896b89500c7cb4567d8d0fd5e0eb
Unknown <oscar.lopez@imaginamos.com.co>
2018-09-07 14:17:39 -0500

    [read-admin] login

--- src/users.js
+++ src/users.js
@@ -4,7 +4,7 @@ import {List, Datagrid, EmailField, TextField} from 'react-admin';
 export const UserList = (props) =>(
   <List title="All users" {...props}>
     <Datagrid>
-      <TextField source="ide" />
+      <TextField source="id" />
       <TextField source="name" />
       <TextField source="username" />
       <EmailField source="email" />
